25b35bf3080eb90fac282cd155f622bcfd8ef6a2
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2022-07-09 12:34:15 -0500

    Starting to make some contributions to the game. It's gonna be a long but exciting journey.

---
+++ Interfaces.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Potato
+{
+    internal interface IComponent : IDrawable, IUpdateable
+    {
+    }
+
+    internal interface IUpdateable
+    {
+        void Update(GameTime gameTime);
+    }
+    internal interface IDrawable
+    {
+        void Draw(SpriteBatch spriteBatch);
+    }
+    internal interface IDestroyable
+    {
+        bool Destroyed { get; }
+        void Destroy();
+    }
+}

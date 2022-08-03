802e3e3de333094102ea03c376ff4cd64b12aa4d
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2022-08-03 00:20:21 -0500

    Working on the title room.

--- World/Room/Title/TitleMenu.cs
+++ World/Room/Title/TitleMenu.cs
@@ -9,10 +9,29 @@ namespace Potato.World.Room.Title
 {
     internal class TitleMenu : IMenu
     {
+        private const float outerWidth = 512f;
+        private const float innerWidth = outerWidth * .90f;
+        private const float dividerWidth = innerWidth * .90f;
+        private ContainerMenu containerMenu;
+        private SelectMenu engineEditorSelectMenu;
+        private SelectMenu optionsSelectMenu;
         public OpenCloseState MenuState => throw new NotImplementedException();
         public IController Controller { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public Vector2 Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public Size2 Size { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+        public TitleMenu()
+        {
+            engineEditorSelectMenu = new SelectMenu(text: "Engine Editor", align: Alignment.Center, width: innerWidth);
+            optionsSelectMenu = new SelectMenu(text: "Options", align: Alignment.Center, width: innerWidth);
+            containerMenu = new ContainerMenu(
+                components: new List<IMenu>()
+                {
+                    engineEditorSelectMenu,
+                    optionsSelectMenu
+                },
+                align: Alignment.Center);
+        }
 
         public void CloseMenu()
         {
